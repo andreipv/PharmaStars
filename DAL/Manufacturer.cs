78f1e8e0e4e47ae3f5d0f19433df66b466ab9325
Andrei Purcar <andrei.purcar@endava.com>
2017-08-08 14:07:25 +0300

    Created the DB

---
+++ DAL/Manufacturer.cs
@@ -0,0 +1,28 @@
+namespace DAL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Manufacturer
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Manufacturer()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int ID { get; set; }
+
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [StringLength(255)]
+        public string Adress { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
