8d6ee846c9cccaedc6ec9423349d733736204f4d
dgherghel <dgherghel@EN610342>
2017-08-08 15:17:05 +0300

    Added price for products + repo interface

---
+++ DAL/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public interface IRepository<T> where T: class
+    {
+        T Get(int key);
+        T Add(T entity);
+        T Delete(T entity);
+        T Update(int key, T updated);
+        IQueryable<T> GetAll();
+    }
+}
