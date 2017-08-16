3cff1483c72d5ce86ec16d8cf39d28c29e63cc22
Andrei Purcar <andrei.purcar@endava.com>
2017-08-16 10:53:58 +0300

    Admin controllers

---
+++ MVC/Controllers/AdminManufacturersController.cs
@@ -0,0 +1,21 @@
+﻿using MVC.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class AdminManufacturersController : Controller
+    {
+        // GET: AdminManufacturers
+        public async Task<ActionResult> Index()
+        {
+            ManufacturerService manServ = new ManufacturerService();
+
+            return View(await manServ.GetAll());
+        }
+    }
+}
---
+++ MVC/Controllers/AdminProductsController.cs
@@ -0,0 +1,24 @@
+﻿using MVC.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class AdminProductsController : Controller
+    {
+        // GET: AdminHome
+        public async Task<ActionResult> Index()
+        {
+            
+
+            ProductsService ps = new ProductsService();
+            
+
+            return View(await ps.GetAll());
+        }
+    }
+}
